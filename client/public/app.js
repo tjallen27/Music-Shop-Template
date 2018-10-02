425a6b14ae2f4565dfde9f4ade42b0f60946fd73
tjallen27 <tjallen91@hotmail.co.uk>
2018-10-02 10:06:26 +0100

    button styling update

--- client/public/app.js
+++ client/public/app.js
@@ -1,14 +1,15 @@
 var buttons = document.getElementsByClassName('StripeCheckout');
-
+const targetBtn = document.getElementById('message-successful-text');
+const downloadLink = document.createElement('a');
+const downloadLinkText = document.createTextNode('Download Here');
+const att = document.createAttribute('download');
 
 for(var i = 0; i < buttons.length; i++){
   buttons[i].innerHTML = 'Buy';
   buttons[i].addEventListener('click', function(event){
+    document.getElementById('message-successful-payment').style.display = "none";
     console.log(event.target.parentNode.parentNode.childNodes[1].childNodes[0]);
-    const targetBtn = document.getElementById('message-successful-text');
-    const downloadLink = document.createElement('a');
-    const downloadLinkText = document.createTextNode('Download Here');
-    const att = document.createAttribute('download');
+
     downloadLink.appendChild(downloadLinkText);
     downloadLink.title = 'Download Link';
     downloadLink.href=event.target.parentNode.parentNode.childNodes[1].childNodes[0].src;
